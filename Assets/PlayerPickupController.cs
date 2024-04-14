82b9c1b58cce48b498ccd9835b646f5a29743c06
Luis Rojas <lrojas94@gmail.com>
2024-04-14 14:46:50 -0400

    Start working on object generators

--- Assets/PlayerPickupController.cs
+++ Assets/PlayerPickupController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerPickupController : MonoBehaviour
 {
+    [SerializeField]
+    private float rayLength = 1.5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +13,19 @@ public class PlayerPickupController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 5f))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.Log("Did Hit");
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * rayLength, Color.white);
+            Debug.Log("Did not Hit");
+        }
     }
 }
